15b6bb100d823ba8933caedc4aadb926924ff008
AvadhutJadhav96 <avadhutjadhav966@gmail.com>
2025-03-15 19:48:49 +0530

    Defined the skeleton of problem

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+
+func main(){
+	//1. Input the name of the file
+	//2. Set the duration of the timer
+	//3. Pull the problems from the file (calling the problem parser function)
+	//4. Handle the error
+	//5. Create a variable to count the correct answers
+	//6. Using the duration of timer, we want to initialize the timer
+	//7. Loop through the problems, print the questions, we will accept the answers
+	//8. We will calculate and print the result
+}
